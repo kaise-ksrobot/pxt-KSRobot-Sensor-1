8764294ea1c00faddd03714ed3abfb8c81097e2d
arthur <arthur@kaise.com.tw>
2020-12-14 15:16:10 +0800

    v0.0.4

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color=#ff4b4b icon="\uf0ee" block="KSRobot_Sensor"
 namespace KSRobot_Sensor {
     export enum DHT11_State {
-         //% block="Celsius"
+        //% block="Celsius"
         DHT11_C,
         //% block="Fahrenheit"
         DHT11_F,
@@ -18,7 +18,7 @@ namespace KSRobot_Sensor {
         let i = pins.digitalReadPin(dht11pin)
         pins.setPull(dht11pin, PinPullMode.PullUp);
         switch (dht11type) {
-            case DHT11_C:
+            case DHT11_State.DHT11_C:
                 let dhtvalue1 = 0;
                 let dhtcounter1 = 0;
                 while (pins.digitalReadPin(dht11pin) == 1);
@@ -38,7 +38,7 @@ namespace KSRobot_Sensor {
                 }
                 return ((dhtvalue1 & 0x0000ff00) >> 8);
                 break;
-            case DHT11_F:
+            case DHT11_State.DHT11_F:
                 while (pins.digitalReadPin(dht11pin) == 1);
                 while (pins.digitalReadPin(dht11pin) == 0);
                 while (pins.digitalReadPin(dht11pin) == 1);
@@ -58,7 +58,7 @@ namespace KSRobot_Sensor {
                 }
                 return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
                 break;
-            case DHT11_H:
+            case DHT11_State.DHT11_H:
                 while (pins.digitalReadPin(dht11pin) == 1);
                 while (pins.digitalReadPin(dht11pin) == 0);
                 while (pins.digitalReadPin(dht11pin) == 1);
