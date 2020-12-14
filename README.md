034d39125a3061fa92bbc5567821f0b89585c2bd
arthur <arthur@kaise.com.tw>
2020-12-14 15:09:41 +0800

    v0.0.2

--- README.md
+++ README.md
@@ -8,6 +8,16 @@
 
 MIT
 
+The code of ultrasonic was coded by Microsoft
+https://github.com/Microsoft/pxt-sonar
+
+The code of DHT11 was coded by MonadnockSystems
+https://github.com/alankrantas/pxt-dht11_dht22
+
+
+The code of IIC 1602 Panel was coded by lioujj
+https://github.com/lioujj/pxt-lcd1602
+
 ## Supported targets
 
 * for PXT/microbit
--- main.ts
+++ main.ts
@@ -1,1 +1,87 @@
+//% color=#ff4b4b icon="\uf0ee" block="KSRobot_Sensor"
+namespace KSRobot_Sensor {
+    export enum DHT11_State {
+         //% block="Celsius"
+        DHT11_C,
+        //% block="Fahrenheit"
+        DHT11_F,
+        //% block="Humidity"
+        DHT11_H,
+    }
 
+ 
+    //% blockId="KSRobot_dht11" block="DHT11 set pin %dht11pin|get %dht11type"
+    export function dht11(dht11type: DHT11_State, dht11pin: DigitalPin): number {
+
+        pins.digitalWritePin(dht11pin, 0)
+        basic.pause(18)
+        let i = pins.digitalReadPin(dht11pin)
+        pins.setPull(dht11pin, PinPullMode.PullUp);
+        switch (dht11type) {
+            case DHT11_C:
+                let dhtvalue1 = 0;
+                let dhtcounter1 = 0;
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter1 = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter1 += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter1 > 2) {
+                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                        }
+                    }
+                }
+                return ((dhtvalue1 & 0x0000ff00) >> 8);
+                break;
+            case DHT11_F:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                let dhtvalue = 0;
+                let dhtcounter = 0;
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter > 2) {
+                            dhtvalue = dhtvalue + (1 << (31 - i));
+                        }
+                    }
+                }
+                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
+                break;
+            case DHT11_H:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+
+                let value = 0;
+                let counter = 0;
+
+                for (let i = 0; i <= 8 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    counter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        counter += 1;
+                    }
+                    if (counter > 3) {
+                        value = value + (1 << (7 - i));
+                    }
+                }
+                return value;
+            default:
+                return 0;
+        }
+    }
+
+
+
+}
