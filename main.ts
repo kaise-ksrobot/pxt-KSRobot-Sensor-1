ac77c5454ad8b347c13a7e8d70eed5175a585f94
arthur <arthur@kaise.com.tw>
2021-04-07 00:13:42 +0800

    v0.1.2

--- main.ts
+++ main.ts
@@ -1,87 +1,108 @@
 //% color=#ff4b4b icon="\uf0ee" block="KSRobot_Sensor"
 namespace KSRobot_Sensor {
-    export enum DHT11_State {
-        //% blockId="Celsius" block="Celsius"
-        DHT11_C,
-        //% blockId="Fahrenheit" block="Fahrenheit"
-        DHT11_F,
-        //% blockId="Humidity" block="Humidity"
-        DHT11_H,
+
+    let _temperature: number = -999.0
+    let _humidity: number = -999.0
+    let _readSuccessful: boolean = false
+
+    export enum DHT_type {
+        //% blockId="DHT11" block="DHT11"
+        DHT11,
+        //% blockId="DHT22" block="DHT22"
+        DHT22,
     }
 
- 
-    //% blockId="KSRobot_dht11" block="DHT11 set pin %dht11pin|get %dht11type"
-    export function dht11(dht11pin: DigitalPin, dht11type: DHT11_State ): number {
+    export enum DHT_State {
+        //% blockId="Celsius" block="Celsius"
+        Celsius,
+        //% blockId="Fahrenheit" block="Fahrenheit"
+        Fahrenheit,
+        //% blockId="Humidity" block="Humidity"
+        Humidity,
+    }
+
+
+    //% blockId="KSRobot_dht11" block="DHT set %dht_type pin %dataPin|get %dhttype"
+    export function dht_readdata(dht_type: DHTtype, dataPin: DigitalPin, dhttype: DHT_State): number {
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        let checksum: number = 0
+        let checksumTmp: number = 0
 
         basic.pause(500)
-        pins.digitalWritePin(dht11pin, 0)
+        pins.digitalWritePin(dataPin, 0)
         basic.pause(18)
-        pins.setPull(dht11pin, PinPullMode.PullUp);
-        pins.digitalReadPin(dht11pin)
-        switch (dht11type) {
-            case DHT11_State.DHT11_C:
-                let dhtvalue1 = 0;
-                let dhtcounter1 = 0;
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
-                for (let i = 0; i <= 32 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    dhtcounter1 = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        dhtcounter1 += 1;
-                    }
-                    if (i > 15) {
-                        if (dhtcounter1 > 2) {
-                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
-                        }
-                    }
+        pins.setPull(dataPin, PinPullMode.PullUp);
+        pins.digitalReadPin(dataPin)
+        control.waitMicros(40)
+
+        while (pins.digitalReadPin(dataPin) == 1);
+        while (pins.digitalReadPin(dataPin) == 0);
+        while (pins.digitalReadPin(dataPin) == 1);
+        //read data (5 bytes)
+        for (let index = 0; index < 40; index++) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            control.waitMicros(28)
+            //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+            if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+        }
+
+
+
+        //convert byte number array to integer
+        for (let index = 0; index < 5; index++)
+            for (let index2 = 0; index2 < 8; index2++)
+                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+        //verify checksum
+        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+        checksum = resultArray[4]
+        if (checksumTmp >= 512) checksumTmp -= 512
+        if (checksumTmp >= 256) checksumTmp -= 256
+        if (checksum == checksumTmp) _readSuccessful = true
+
+        //read data if checksum ok
+        if (_readSuccessful) {
+            if (dht_type == DHT_type.DHT11) {
+                //DHT11
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+            } else {
+                //DHT22
+                let temp_sign: number = 1
+                if (resultArray[2] >= 128) {
+                    resultArray[2] -= 128
+                    temp_sign = -1
                 }
+                _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
+                _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign
+            }
+           
                 
-                return ((dhtvalue1 & 0x0000ff00) >> 8);
-                break;
-            case DHT11_State.DHT11_F:
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
-                let dhtvalue = 0;
-                let dhtcounter = 0;
-                for (let i = 0; i <= 32 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    dhtcounter = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        dhtcounter += 1;
-                    }
-                    if (i > 15) {
-                        if (dhtcounter > 2) {
-                            dhtvalue = dhtvalue + (1 << (31 - i));
-                        }
-                    }
-                }
-                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
-                break;
-            case DHT11_State.DHT11_H:
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
+        }
 
-                let value = 0;
-                let counter = 0;
 
-                for (let i = 0; i <= 8 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    counter = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        counter += 1;
-                    }
-                    if (counter > 3) {
-                        value = value + (1 << (7 - i));
-                    }
-                }
-                return value;
+        switch (dhttype) {
+            case DHT_State.Celsius:
+
+                return _temperature;
+                break;
+
+            case DHT_State.Fahrenheit:
+
+                return _temperature = _temperature * 9 / 5 + 32;
+                break;
+
+            case DHT_State.Humidity:
+
+                return _humidity;
+                break;
+
             default:
                 return 0;
         }
+
     }
 
 
